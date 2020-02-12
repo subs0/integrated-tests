86e62e9c8be51347cbe62b598a58f8dd6559e557
Logan Powell <loganp@tepper.cmu.edu>
2020-02-12 16:12:40 -0500

    test

--- scripts/update.js
+++ scripts/update.js
@@ -3,7 +3,7 @@ const { exec, args } = require("./utils")
 const msg = `"typecasting..."`
 
 const update = ({ st }) => {
-  exec(`"cd src/${st} && npm run git -- test"`, { stdio: [0, 1, 2] })
+  exec(`npm run --prefix=src/${st} && npm run git -- test`, { stdio: [0, 1, 2] })
 }
 console.log(msg)
 update(args(msg))
