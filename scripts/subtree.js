210567e34437dea23c854432cd4ed1658ac08e54
Logan Powell <loganp@tepper.cmu.edu>
2020-02-12 15:18:20 -0500

    syncing supertree with subtree...

--- scripts/subtree.js
+++ scripts/subtree.js
@@ -1,6 +1,6 @@
 const { exec, args } = require("./utils")
 
-const msg = "syncing super/subtree..."
+const name = `"syncing supertree with subtree..."`
 
 const subtree = ({ st, via, br, msg }) => {
   console.log(msg, st, via, br)
@@ -12,7 +12,7 @@ const subtree = ({ st, via, br, msg }) => {
   )
 }
 
-subtree(args(msg))
+subtree(args(name))
 
 // CLI use
 // node scripts\subtree.js st=spool via=push msg="pushing to subtree" br="master"
