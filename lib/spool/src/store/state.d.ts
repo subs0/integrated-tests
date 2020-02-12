b1fe6141d16e63db8374d2f5f5e5f2a0ea90ea28
Logan Powell <loganp@tepper.cmu.edu>
2020-02-12 18:02:25 -0500

    syncing supertree with subtree

--- lib/spool/src/store/state.d.ts
+++ lib/spool/src/store/state.d.ts
@@ -1,3 +1,2 @@
-import { Atom } from "@thi.ng/atom";
-export declare const $store$: Atom<Object>;
-export declare const set$$tate: (path: any, val: any, store?: Atom<Object>) => Object;
+export declare const $store$: any;
+export declare const set$$tate: (path: any, val: any, store?: any) => any;
