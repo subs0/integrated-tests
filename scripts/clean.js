78f59268ec8f6abba9cbc570322ee0041ed69b1b
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 18:05:59 -0500

    syncing supertree with subtree

---
+++ scripts/clean.js
@@ -0,0 +1,11 @@
+const { exec, args } = require("./utils")
+
+const msg = "cleaning lib"
+
+const clean = ({ st }) => {
+  exec(`cd src/${st} && rm -rf lib`, { stdio: [0, 1, 2] })
+}
+clean(args(msg))
+
+// CLI use
+// node scripts\types st=spool
