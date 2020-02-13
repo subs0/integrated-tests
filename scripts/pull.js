9eaafcbac63cd507a6d46fec300332fde5867aa6
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 16:54:17 -0500

    trying again

---
+++ scripts/pull.js
@@ -0,0 +1,11 @@
+const { exec, args } = require("./utils")
+
+const msg = "switching to subtree directory git pulling updates"
+
+const pull = ({ st }) => {
+  exec(`cd src/${st} && git pull origin master"`, { stdio: [0, 1, 2] })
+}
+pull(args(msg))
+
+// CLI use
+// node scripts\types st=spool
