9bb7ae491cfdd551fdd253323a2e3886c3cde9a5
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 17:15:07 -0500

    syncing supertree with subtree

--- scripts/subtree.js
+++ scripts/subtree.js
@@ -2,11 +2,11 @@ const { exec, args } = require("./utils")
 
 const msg = "syncing supertree with subtree"
 
-const subtree = ({ st, via, br, msg, f }) => {
+const subtree = ({ st, via, br, msg }) => {
   exec(
     `git add . && ` +
       `git commit -m "${msg}" && ` +
-      `git subtree ${via} ${f ? "-f" : ""} --prefix=src/${st} ${st} ${br}`,
+      `git subtree ${via} --prefix=src/${st} ${st} ${br}`,
     { stdio: [0, 1, 2] }
   )
 }
