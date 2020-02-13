99648d0314ac592323bed16242c1d8aed2db7b30
Logan Powell <loganp@tepper.cmu.edu>
2020-02-13 10:16:41 -0500

    updates dom

--- lib/registers/index.d.ts
+++ lib/registers/index.d.ts
@@ -1,2 +1,4 @@
+import { BootCFG } from "@-0/keys";
 export declare const registerRouterDOM: (router: any) => any;
-export declare const boot: (CFG: Object) => void;
+export declare const pair: (store: any, CMDS?: any[]) => void;
+export declare const boot: (CFG: BootCFG) => void;
