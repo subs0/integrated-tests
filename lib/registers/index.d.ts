43fb1f19ee84f01170b3ccc45b18e4378ece5822
Logan Powell <loganp@tepper.cmu.edu>
2020-02-14 23:24:41 -0500

    switching to subtree directory and running updates

--- lib/registers/index.d.ts
+++ lib/registers/index.d.ts
@@ -1,5 +1,3 @@
 import { BootCFG, Command } from "@-0/keys";
-export declare const registerRouterCMD: Command;
-export declare const registerRouterDOM: (router: any) => any;
-export declare const registerDOMrouterCMD: (router: any) => Command;
+export declare const registerRouterDOM: (router: any) => Command;
 export declare const boot: (CFG: BootCFG) => void;
