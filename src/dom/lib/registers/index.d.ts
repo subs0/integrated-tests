737d65814d50ad8b6166740ec5c14da5d399eedf
Logan Powell <loganp@tepper.cmu.edu>
2020-02-14 22:07:53 -0500

    switching to subtree directory and running updates

--- src/dom/lib/registers/index.d.ts
+++ src/dom/lib/registers/index.d.ts
@@ -1,7 +1,5 @@
-import { IAtom } from "@thi.ng/atom";
 import { BootCFG, Command } from "@-0/keys";
 export declare const registerRouterCMD: Command;
 export declare const registerRouterDOM: (router: any) => any;
 export declare const registerDOMrouterCMD: (router: any) => Command;
-export declare const pair: (globalStore?: IAtom<Object>, Commands?: Command[]) => ((CFG: BootCFG) => void)[];
 export declare const boot: (CFG: BootCFG) => void;
