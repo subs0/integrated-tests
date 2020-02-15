097d6dac4232be1047d21aed16d6ef560f3071b5
Logan Powell <loganp@tepper.cmu.edu>
2020-02-14 23:28:51 -0500

    switching to subtree directory and running updates

---
+++ src/dom/lib/commands/routing.d.ts
@@ -0,0 +1,5 @@
+export declare const HURLer: (ev: any) => any;
+export declare const HURL: any;
+export declare const SET_LINK_ATTRS_DOM: any;
+export declare const HREF_PUSHSTATE_DOM: any;
+export declare const NOTIFY_PRERENDER_DOM: any;
