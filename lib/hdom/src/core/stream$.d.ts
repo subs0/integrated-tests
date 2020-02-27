575df7a9e3fced15d6d62f2c5139771b46b020ad
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 14:35:33 -0500

    updates

--- lib/hdom/src/core/stream$.d.ts
+++ lib/hdom/src/core/stream$.d.ts
@@ -2,6 +2,6 @@ import { URL_FULL, DOM_NODE } from "@-0/keys";
 export declare const popstate$: any;
 export declare const DOMContentLoaded$: any;
 export declare const DOMnavigated$: import("@thi.ng/rstream").Subscription<unknown, {
-    [URL_FULL]: any;
-    [DOM_NODE]: any;
+    URL: any;
+    NODE: any;
 }>;
--- lib/hdom/src/tasks/routing.d.ts
+++ lib/hdom/src/tasks/routing.d.ts
@@ -1,1 +1,2 @@
+export declare const URL__ROUTE: (CFG: Object | Function) => any;
 export declare const URL_DOM__ROUTE: (CFG: any) => (acc: any) => any[];
