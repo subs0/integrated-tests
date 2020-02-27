d92f9fb4a351e026e6bcdebb9ede1d802504477f
Logan Powell <loganp@tepper.cmu.edu>
2020-02-27 14:32:07 -0500

    switching to subtree directory and running updates

---
+++ lib/commands/head.d.ts
@@ -0,0 +1,1 @@
+export declare const INJECT_HEAD: any;
---
+++ lib/store/state.d.ts
@@ -0,0 +1,21 @@
+import { Atom } from "@thi.ng/atom";
+export declare const $store$: Atom<{
+    _ROUTE_PATH: any[];
+    _ROUTE_LOADING: boolean;
+    _PAGE_TEMPLATE: any;
+    _ROOT: HTMLElement;
+    _COMMANDS: {};
+}>;
+export declare const set$$tate: (path: any, val: any, store?: Atom<{
+    _ROUTE_PATH: any[];
+    _ROUTE_LOADING: boolean;
+    _PAGE_TEMPLATE: any;
+    _ROOT: HTMLElement;
+    _COMMANDS: {};
+}>) => {
+    _ROUTE_PATH: any[];
+    _ROUTE_LOADING: boolean;
+    _PAGE_TEMPLATE: any;
+    _ROOT: HTMLElement;
+    _COMMANDS: {};
+};
