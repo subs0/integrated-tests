2afd2b71111ce563eb6904f9aeb7c5c029414fbd
Logan Powell <loganp@tepper.cmu.edu>
2020-04-25 16:48:24 -0400

    syncing supertree with subtree

--- src/hdom/lib/store/state.d.ts
+++ src/hdom/lib/store/state.d.ts
@@ -3,19 +3,19 @@ export declare const $store$: Atom<{
     _ROUTE_PATH: any[];
     _ROUTE_LOADING: boolean;
     _PAGE_TEMPLATE: any;
-    _ROOT: HTMLElement;
+    _DOM_ROOT: any;
     _COMMANDS: {};
 }>;
 export declare const set$$tate: (path: any, val: any, store?: Atom<{
     _ROUTE_PATH: any[];
     _ROUTE_LOADING: boolean;
     _PAGE_TEMPLATE: any;
-    _ROOT: HTMLElement;
+    _DOM_ROOT: any;
     _COMMANDS: {};
 }>) => {
     _ROUTE_PATH: any[];
     _ROUTE_LOADING: boolean;
     _PAGE_TEMPLATE: any;
-    _ROOT: HTMLElement;
+    _DOM_ROOT: any;
     _COMMANDS: {};
 };
