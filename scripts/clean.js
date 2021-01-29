7504ba1c108a9aa738b0dbbed7eae789c13cf6ce
Logan Powell <loganp@tepper.cmu.edu>
2021-01-29 14:19:49 -0500

    syncing supertree with subtree

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,7 +2,7 @@ import { exec, args } from "./utils.js"
 
 const msg = "cleaning lib"
 
-// cleans out the prior typescript compile and checks for updated packages
+// cleans out the prior typescript compile and updates outdated packages
 const clean = ({ st }) => {
     exec(`cd src/${st} && rm -rf lib && npm run ncu`, { stdio: [ 0, 1, 2 ] })
 }
