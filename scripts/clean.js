6e8096a5d85786f5b0e099d4b88c4ce7331516d9
Logan Powell <loganp@tepper.cmu.edu>
2021-01-29 14:10:24 -0500

    syncing supertree with subtree

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,8 +2,9 @@ import { exec, args } from "./utils.js"
 
 const msg = "cleaning lib"
 
+// cleans out the prior typescript compile and checks for updated packages
 const clean = ({ st }) => {
-  exec(`cd src/${st} && rm -rf lib`, { stdio: [0, 1, 2] })
+    exec(`cd src/${st} && rm -rf lib && npm run ncu`, { stdio: [ 0, 1, 2 ] })
 }
 clean(args(msg))
 
--- scripts/update.js
+++ scripts/update.js
@@ -2,8 +2,9 @@ import { exec, args } from "./utils.js"
 
 const msg = "switching to subtree directory and running updates"
 
+// checks into the child repo and runs its `git` script in its package.json
 const update = ({ st, msg }) => {
-  exec(`cd src/${st} && npm run git -- "${msg}"`, { stdio: [0, 1, 2] })
+    exec(`cd src/${st} && npm run git -- "${msg}"`, { stdio: [ 0, 1, 2 ] })
 }
 update(args(msg))
 
