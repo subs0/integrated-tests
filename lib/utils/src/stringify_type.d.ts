45cd886c02a2d1bdcbe993829e0875107e33cc71
Logan Powell <loganp@tepper.cmu.edu>
2021-02-11 11:36:10 -0500

    syncing supertree with subtree

--- lib/utils/src/stringify_type.d.ts
+++ lib/utils/src/stringify_type.d.ts
@@ -1,1 +1,1 @@
-export declare const stringify_type: (x: any) => "UNKNOWN" | "NULLARY" | "UNARY" | "BINARY" | "N-ARY" | "PROMISE" | "ARRAY" | "OBJECT" | "ERROR" | "PRIMITIVE";
+export declare const stringify_type: (x: any) => "NULLARY" | "UNARY" | "BINARY" | "N-ARY" | "PROMISE" | "ARRAY" | "OBJECT" | "ERROR" | "PRIMITIVE" | "UNKNOWN";
