6cf33e003aa2d19333a7dad2c3a0c398a67b692c
Logan Powell <loganp@tepper.cmu.edu>
2021-02-11 18:04:34 -0500

    new error string for missing props in registered commands + routing command tests

--- src/browser/tests/registers/routing.test.js
+++ src/browser/tests/registers/routing.test.js
@@ -19,6 +19,8 @@ import { log$, out$, run$, cmd$ } from "@-0/spool"
 import { URL2obj } from "@-0/utils"
 import { registerRouterDOM } from "../../src/registers"
 
+const warned = (x = jest.fn()) => (jest.spyOn(console, "warn").mockImplementation(x), x)
+
 describe("registerRouterDOM", () => {
     const router_fn = url => ({ [URL_DATA]: true, [URL_PAGE]: 1 })
     //const router_obj = {
@@ -31,26 +33,31 @@ describe("registerRouterDOM", () => {
     const ROUTER = registerRouterDOM(router_fn)
     const spy = jest.fn(x => x)
     out$.subscribeTopic("_URL_NAVIGATED$_DOM", { next: spy, error: console.warn })
-    test("popstate events trigger _URL_NAVIGATED$_DOM Command (routing)", () => {
+
+    const missing_props = warned()
+
+    test("1: popstate events trigger _URL_NAVIGATED$_DOM Command (routing)", () => {
         fireEvent(window, createEvent("popstate", window))
         const result = spy.mock.results[0].value
         const sub$ = result[CMD_SUB$]
         const url = result[CMD_ARGS][URL_FULL]
 
-        expect(spy).toHaveBeenCalledTimes(1)
+        //expect(missing_props.mock.calls.length).toBe(1)
+        expect(spy.mock.calls.length).toBe(1)
         expect({ [CMD_SUB$]: sub$, [URL_FULL]: url }).toMatchObject({
             [CMD_SUB$] : "_URL_NAVIGATED$_DOM",
             [URL_FULL] : "http://localhost/"
         })
     })
 
-    test("DOMContentLoaded events trigger _URL_NAVIGATED$_DOM Command (routing)", () => {
+    test("2: DOMContentLoaded events trigger _URL_NAVIGATED$_DOM Command (routing)", () => {
         fireEvent(window, createEvent("DOMContentLoaded", window))
         const result = spy.mock.results[1].value
         const sub$ = result[CMD_SUB$]
         const url = result[CMD_ARGS][URL_FULL]
 
-        expect(spy).toHaveBeenCalledTimes(2)
+        //expect(missing_props.mock.calls.length).toBe(1)
+        expect(spy.mock.calls.length).toBe(2)
         expect({ [CMD_SUB$]: sub$, [URL_FULL]: url }).toMatchObject({
             [CMD_SUB$] : "_URL_NAVIGATED$_DOM",
             [URL_FULL] : "http://localhost/"
