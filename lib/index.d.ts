29c6536c6872f173ffc500aec4fe05087021f037
Logan Powell <loganp@tepper.cmu.edu>
2021-02-13 09:58:22 -0500

    syncing supertree with subtree

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 export * from "./hdom/lib";
-export * from "./browser/node_modules/@-0/utils/lib";
+export * from "@-0/utils";
 export * from "./browser/node_modules/@-0/spool/lib";
 export { keys };
 import * as keys from "./hdom/node_modules/@-0/keys/lib";
