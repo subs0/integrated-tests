8bfac1dc7680857e91d1353bf9e554c57eb28206
Logan Powell <loganp@tepper.cmu.edu>
2021-02-14 11:44:28 -0500

    moar tests

---
+++ tests/commands/setstate.test.js
@@ -0,0 +1,7 @@
+import { stream } from "@thi.ng/rstream"
+import { run, out$ } from "@-0/spool"
+import { SET_STATE } from "../../src/commands"
+
+describe("Command: SET_STATE", () => {
+    test("")
+})
