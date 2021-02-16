04bcf0f8ce943d2d53e280b196e5f2199b177d0f
Logan Powell <loganp@tepper.cmu.edu>
2021-02-16 12:30:56 -0500

    switching to subtree directory and running updates

--- lib/store/state.d.ts
+++ lib/store/state.d.ts
@@ -4,18 +4,15 @@ export declare const $store$: Atom<{
     $$_LOAD: boolean;
     $$_VIEW: any;
     $$_ROOT: any;
-    $$_CMDS: {};
 }>;
 export declare const set$$tate: (path: any, val: any, store?: Atom<{
     $$_PATH: any[];
     $$_LOAD: boolean;
     $$_VIEW: any;
     $$_ROOT: any;
-    $$_CMDS: {};
 }>) => {
     $$_PATH: any[];
     $$_LOAD: boolean;
     $$_VIEW: any;
     $$_ROOT: any;
-    $$_CMDS: {};
 };
