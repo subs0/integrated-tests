3302a77315173f341a3ea94bec86731bc9f93e84
Logan Powell <loganp@tepper.cmu.edu>
2021-02-16 16:54:22 -0500

    switching to subtree directory and running updates

--- src/commands/setstate.ts
+++ src/commands/setstate.ts
@@ -1,7 +1,7 @@
 /**
  * @module commands/state
  */
-import { CMD_SUB$, CMD_ARGS, CMD_WORK, STATE_DATA, STATE_PATH, Command } from "@-0/keys"
+import { CMD_SUB$, CMD_ARGS, CMD_WORK, SET_DATA, SET_PATH, Command } from "@-0/keys"
 import { set$$tate, $store$ } from "../store"
 import { registerCMD } from "@-0/spool"
 import { Err_missing_props } from "@-0/utils"
@@ -16,11 +16,11 @@ export const createSetStateCMD: Command = store =>
         [CMD_SUB$]: "_SET_STATE",
         [CMD_ARGS]: x => x,
         [CMD_WORK]: args => {
-            const path = args[STATE_PATH]
-            const data = args[STATE_DATA]
+            const path = args[SET_PATH]
+            const data = args[SET_DATA]
             const props = {
-                [STATE_PATH]: path,
-                [STATE_DATA]: data
+                [SET_PATH]: path,
+                [SET_DATA]: data
             }
             if (path && data !== undefined) return set$$tate(path, data, store)
             console.warn(Err_missing_props("_SET_STATE", props, args))
