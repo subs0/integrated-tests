4a68f3c5f298327740573dedb8d1e6f821cdc585
Logan Powell <loganp@tepper.cmu.edu>
2021-02-17 13:12:15 -0500

    syncing supertree with subtree

--- lib/browser/src/tasks/routing.d.ts
+++ lib/browser/src/tasks/routing.d.ts
@@ -1,2 +1,3 @@
-export declare const URL__ROUTE: (CFG: Function | Object) => any;
+import { RouterCFG, Router } from "@-0/keys";
+export declare const URL__ROUTE: (CFG: Router | RouterCFG) => any;
 export declare const URL_DOM__ROUTE: (CFG: any) => (ACC: any) => any[];
