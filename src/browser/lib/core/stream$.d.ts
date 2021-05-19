e1d259f42dd973fa79f0719d05c561ba82af2deb
Logan Powell <loganp@tepper.cmu.edu>
2021-05-19 10:44:52 -0400

    switching to subtree directory and running updates

--- src/browser/lib/core/stream$.d.ts
+++ src/browser/lib/core/stream$.d.ts
@@ -1,7 +1,6 @@
-import { URL_FULL, DOM_NODE } from "@-0/keys";
 export declare const popstate$: any;
 export declare const DOMContentLoaded$: any;
-export declare const DOMnavigated$: import("@thi.ng/rstream").Subscription<unknown, {
+export declare const DOMnavigated$: import("@thi.ng/rstream").ISubscription<unknown, {
     URL_FULL: any;
     DOM_NODE: any;
 }>;
