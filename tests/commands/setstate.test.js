b1ea74769417d830597fcfa8c55f1b5cf60462ef
Logan Powell <loganp@tepper.cmu.edu>
2021-07-10 13:27:59 -0400

    tiny updates

--- tests/commands/setstate.test.js
+++ tests/commands/setstate.test.js
@@ -3,7 +3,7 @@ import { Atom } from "@thi.ng/atom"
 import { run, out$ } from "@-0/spool"
 import { createSetStateCMD, SET_STATE } from "../../src/commands"
 import { $store$ } from "../../src/store"
-import { CMD_ARGS, SET_DATA, SET_PATH } from "@-0/keys"
+import { CMD_ARGS, STATE_DATA, STATE_PATH } from "@-0/keys"
 
 describe("setstate", () => {
     test("1: `createSetStateCMD` state/store input is used as state container", () => {
@@ -11,14 +11,17 @@ describe("setstate", () => {
         const CUSTOM_STATE_SETTING_COMMAND = createSetStateCMD(store)
         // before
         expect(store.deref()).toMatchObject({ hello: null })
-        out$.next({ ...CUSTOM_STATE_SETTING_COMMAND, [CMD_ARGS]: { [SET_PATH]: [ "hello" ], [SET_DATA]: "yay! 👏" } })
+        out$.next({
+            ...CUSTOM_STATE_SETTING_COMMAND,
+            [CMD_ARGS]: { [STATE_PATH]: [ "hello" ], [STATE_DATA]: "yay! 👏" }
+        })
         // after
         expect(store.deref()).toMatchObject({ hello: "yay! 👏" })
     })
     test("2: `SET_STATE` Command: applies submitted state changes to global `$store$`", () => {
         // before
         expect($store$.deref()).toMatchObject({})
-        out$.next({ ...SET_STATE, [CMD_ARGS]: { [SET_PATH]: [], [SET_DATA]: "hello" } })
+        out$.next({ ...SET_STATE, [CMD_ARGS]: { [STATE_PATH]: [], [STATE_DATA]: "hello" } })
         // after
         expect($store$.deref()).toBe("hello")
     })
