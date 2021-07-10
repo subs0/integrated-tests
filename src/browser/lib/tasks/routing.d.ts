51334eb44d1973c3052e22f04ef6878b16e24601
Logan Powell <loganp@tepper.cmu.edu>
2021-07-10 10:25:49 -0400

    switching to subtree directory and running updates

--- src/browser/lib/tasks/routing.d.ts
+++ src/browser/lib/tasks/routing.d.ts
@@ -1,31 +1,41 @@
 import { RouterCFG, Router } from "@-0/keys";
 export declare const URL__ROUTE: (CFG: Router | RouterCFG) => any;
 export declare const NOTIFY_PRERENDER_DOM: {
-    sub$: any;
+    sub$: string;
     args: any;
-    reso: any;
-    erro: any;
+    reso: (acc: {}, res: {}) => any;
+    erro: (acc: {}, err: Error, out$: import("@thi.ng/rstream").ISubscribable<any>) => any;
 } | {
-    sub$: any;
+    sub$: string;
     args: any;
     reso?: undefined;
     erro?: undefined;
 };
 export declare const SET_LINK_ATTRS_DOM: {
-    sub$: any;
+    sub$: string;
     args: any;
-    reso: any;
-    erro: any;
+    reso: (acc: {}, res: {}) => any;
+    erro: (acc: {}, err: Error, out$: import("@thi.ng/rstream").ISubscribable<any>) => any;
 } | {
-    sub$: any;
+    sub$: string;
     args: any;
     reso?: undefined;
     erro?: undefined;
 };
 export declare const URL_DOM__ROUTE: (CFG: any) => (ACC: any) => ({
+    sub$: string;
+    args: any;
+    reso: (acc: {}, res: {}) => any;
+    erro: (acc: {}, err: Error, out$: import("@thi.ng/rstream").ISubscribable<any>) => any;
+} | {
+    sub$: string;
+    args: any;
+    reso?: undefined;
+    erro?: undefined;
+} | {
     args: {
-        STATE_PATH: string[];
-        STATE_DATA: boolean;
+        STE_PATH: string[];
+        STE_DATA: boolean;
     };
     apply(this: Function, thisArg: any, argArray?: any): any;
     call(this: Function, thisArg: any, ...argArray: any[]): any;
@@ -39,28 +49,18 @@ export declare const URL_DOM__ROUTE: (CFG: any) => (ACC: any) => ({
     [Symbol.hasInstance](value: any): boolean;
 } | {
     args: {
-        STATE_PATH: string[];
-        STATE_DATA: boolean;
+        STE_PATH: string[];
+        STE_DATA: boolean;
     };
     sub$?: string;
     reso?: (acc: {}, res: {}) => any;
     erro?: (acc: {}, err: Error, out$: import("@thi.ng/rstream").ISubscribable<any>) => any;
     work?: (args: any) => any;
     src$?: import("@thi.ng/rstream").ISubscribable<any>;
-} | {
-    sub$: any;
-    args: any;
-    reso: any;
-    erro: any;
-} | {
-    sub$: any;
-    args: any;
-    reso?: undefined;
-    erro?: undefined;
 } | ((ACC: any) => any) | {
     args: (acc: any) => {
-        STATE_PATH: any;
-        STATE_DATA: any;
+        STE_PATH: any;
+        STE_DATA: any;
     };
     apply(this: Function, thisArg: any, argArray?: any): any;
     call(this: Function, thisArg: any, ...argArray: any[]): any;
@@ -74,8 +74,8 @@ export declare const URL_DOM__ROUTE: (CFG: any) => (ACC: any) => ({
     [Symbol.hasInstance](value: any): boolean;
 } | {
     args: (acc: any) => {
-        STATE_PATH: any;
-        STATE_DATA: any;
+        STE_PATH: any;
+        STE_DATA: any;
     };
     sub$?: string;
     reso?: (acc: {}, res: {}) => any;
