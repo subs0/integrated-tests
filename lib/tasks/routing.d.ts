0d871998f96a872c615fba86309a75d9d79d18b2
Logan Powell <loganp@tepper.cmu.edu>
2021-07-19 15:54:58 -0400

    switching to subtree directory and running updates

--- lib/tasks/routing.d.ts
+++ lib/tasks/routing.d.ts
@@ -1,6 +1,6 @@
-import { RouterCFG, Router, Task } from "@-0/keys";
-export declare const URL__ROUTE: (CFG: Router | RouterCFG) => Task;
+import { RouterCFG, Router, HOTask } from "@-0/keys";
+export declare const URL__ROUTE: (CFG: Router | RouterCFG) => HOTask;
 export declare const NOTIFY_PRERENDER_DOM: import("@-0/keys").Command;
 export declare const SET_LINK_ATTRS_DOM: import("@-0/keys").Command;
 export declare const HREF_PUSHSTATE_DOM: import("@-0/keys").Command;
-export declare const URL_DOM__ROUTE: (CFG: Router | RouterCFG) => Task;
+export declare const URL_DOM__ROUTE: (CFG: Router | RouterCFG) => HOTask;
