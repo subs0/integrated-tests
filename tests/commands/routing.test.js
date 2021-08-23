18da14f9c339a8f1a299408e6fe5a72f4e4b7009
Logan Powell <loganp@tepper.cmu.edu>
2021-08-23 11:03:22 -0400

    syncing supertree with subtree

--- tests/commands/routing.test.js
+++ tests/commands/routing.test.js
@@ -1,35 +1,28 @@
 import { createEvent, fireEvent, getByText } from "@testing-library/dom"
-import { EquivMap } from "@thi.ng/associative"
-import { map } from "@thi.ng/transducers"
+//import { EquivMap } from "@thi.ng/associative"
+//import { map } from "@thi.ng/transducers"
 
 import { URL_FULL, DOM_NODE, CMD_ARGS } from "@-0/keys"
 import { run$, registerCMD } from "@-0/spool"
 import { DOMnavigated$ } from "../../src/core"
 import {
-    FLIP_FIRST,
-    FLIP_LAST_INVERSE_PLAY,
-    cmd_href_pushstate_dom,
-    cmd_inject_head,
-    cmd_notify_prerender_dom,
-    cmd_set_link_attrs_dom,
-    SET_STATE,
-    createSetStateCMD,
+    //cmd_inject_head,
     cmd_nav,
-    navEventHandler
+    navEventHandler,
+    _HREF_PUSHSTATE_DOM,
+    _NOTIFY_PRERENDER_DOM,
+    _SET_LINK_ATTRS_DOM,
 } from "../../src/commands"
 
-const HREF_PUSHSTATE_DOM = registerCMD(cmd_href_pushstate_dom)
-const NOTIFY_PRERENDER_DOM = registerCMD(cmd_notify_prerender_dom)
-const SET_LINK_ATTRS_DOM = registerCMD(cmd_set_link_attrs_dom)
 const NAV = registerCMD(cmd_nav)
-const INJECT_HEAD = registerCMD(cmd_inject_head)
+//const INJECT_HEAD = registerCMD(cmd_inject_head)
 
 describe("Commands: routing", () => {
     test("1: navEventHandler: relative link click triggers DOMnavigated$ injection", () => {
         const spy = jest.fn(x => x)
         DOMnavigated$.subscribe({ next: spy })
 
-        let a = document.createElement("a")
+        const a = document.createElement("a")
         a.href = "/test/navEventHandler/1"
         a.addEventListener("click", navEventHandler)
 
@@ -42,7 +35,7 @@ describe("Commands: routing", () => {
         const spy = jest.fn(x => x)
         DOMnavigated$.subscribe({ next: spy })
 
-        let a = document.createElement("a")
+        const a = document.createElement("a")
         a.href = "https://hello.world/test/navEventHandler/2"
         a.addEventListener("click", navEventHandler)
 
@@ -57,10 +50,10 @@ describe("Commands: routing", () => {
         const spy = jest.fn(x => x)
         DOMnavigated$.subscribe({ next: spy })
         const sim_event = href => ({
-            currentTarget : { document: null },
-            target        : {
-                href
-            }
+            currentTarget: { document: null },
+            target: {
+                href,
+            },
         })
         run$.next({ ...NAV, [CMD_ARGS]: sim_event("/test-NAV-3") })
 
@@ -70,18 +63,18 @@ describe("Commands: routing", () => {
         expect(result).toMatchObject({ [URL_FULL]: "/test-NAV-3" })
     })
     test("4: SET_LINK_ATTRS_DOM Command: dispatch marks clicked links visited/active", () => {
-        let div = document.createElement("div")
+        const div = document.createElement("div")
         div.innerHTML = `
             <a href="/hello" >check it</a>
         `
-        let a = div.querySelector("a")
+        const a = div.querySelector("a")
         //a.href = "https://hello.world/earth"
         a.addEventListener("click", e => {
             run$.next({
-                ...SET_LINK_ATTRS_DOM,
-                [CMD_ARGS] : {
-                    [DOM_NODE] : e.currentTarget
-                }
+                ..._SET_LINK_ATTRS_DOM,
+                [CMD_ARGS]: {
+                    [DOM_NODE]: e.currentTarget,
+                },
             })
         })
 
@@ -92,30 +85,30 @@ describe("Commands: routing", () => {
         //expect(result).toMatchObject({ [DOM_NODE]: a, [URL_FULL]: "https://hello.world/earth" })
     })
     test("5: HREF_PUSHSTATE_DOM Command: if DOM ref is <a> -> history.pushState", () => {
-        let div = document.createElement("div")
+        const div = document.createElement("div")
         div.innerHTML = `
             <a href="/world" >hello</a>
         `
-        let a = div.querySelector("a")
+        const a = div.querySelector("a")
         a.addEventListener("click", e => {
             //console.log("e.target:", e.target)
             run$.next({
-                ...cmd_href_pushstate_dom,
-                [CMD_ARGS] : {
-                    [DOM_NODE] : e.target,
-                    [URL_FULL] : e.target.href
-                }
+                ..._HREF_PUSHSTATE_DOM,
+                [CMD_ARGS]: {
+                    [DOM_NODE]: e.target,
+                    [URL_FULL]: e.target.href,
+                },
             })
         })
-        let before = history.length
+        const before = history.length
         fireEvent(a, createEvent("click", a))
-        let after = history.length
+        const after = history.length
         expect(after - before).toBe(1)
     })
     test("6: NOTIFY_PRERENDER_DOM Command: dispatches trigger for prerender (e.g., rendertron)", () => {
         let triggered = false
-        document.addEventListener("rendered", e => (triggered = true))
-        run$.next(NOTIFY_PRERENDER_DOM)
+        document.addEventListener("rendered", () => (triggered = true))
+        run$.next(_NOTIFY_PRERENDER_DOM)
         expect(triggered).toBe(true)
     })
 })
