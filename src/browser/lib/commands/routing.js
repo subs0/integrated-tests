9cece55b92543df7216c2f73c3f7958efe6d0c91
Logan Powell <loganp@tepper.cmu.edu>
2021-09-11 14:31:29 -0400

    browser: pushstate for all navigation events

--- src/browser/lib/commands/routing.js
+++ src/browser/lib/commands/routing.js
@@ -63,7 +63,7 @@ export const _HREF_PUSHSTATE_DOM = registerCMD({
             [URL_FULL]: url,
             [DOM_NODE]: node,
         };
-        if (url && node && !node.document) {
+        if (url && node) {
             return history.pushState(getScrollPos(), document.title, url);
         }
         if (!url || !node) {
