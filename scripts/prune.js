948bf6b68f2e6305b2b841dab2f2c81a233d026a
Logan Powell <loganp@tepper.cmu.edu>
2022-09-03 00:09:33 -0400

    git cache cleared

---
+++ scripts/prune.js
@@ -0,0 +1,11 @@
+import { exec, args } from "./utils.js"
+
+const msg = "switching to subtree directory and running updates"
+
+const patch = ({ st }) => {
+    exec(`cd src/${st} && npm prune`, { stdio: [0, 1, 2] })
+}
+patch(args(msg))
+
+// CLI use
+// node scripts\types st=spool
